010ea0af7d94310d52c934d33d20686a7c0994b5
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-16 17:17:22 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,23 +1,12 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.wrap(Cypress.env())
-    cy.log('**name:** ' + Cypress.env());
-    console.log(Cypress.env())
-    cy.visit('https://www.google.com/');
-    cy.get('textarea[type="search"]').type('1')
-  });
   it('has valid env values', () => {
-  expect(Cypress.env())
-    .to.be.an('object')
-    .and.to.include.keys('person', 'location')
-  cy.wrap(Cypress.env('person'))
-    .should('have.keys', 'name', 'age')
-    .its('age')
-    .should('be.within', 10, 99)
-  cy.log('**name:** ' + Cypress.env('person').name)
-  cy.log('**city:** ' + Cypress.env('location').city)
-  // save a screenshot of the test runner
-  // to show what the env values were
-  cy.screenshot('env', { capture: 'runner' })
+    expect(Cypress.env())
+      .to.be.an('object')
+      .and.to.include.keys('login', 'password');
+    cy.log('**login:** ' + Cypress.env('login'))
+    cy.log('**password:** ' + Cypress.env('password'))
+    cy.visit('https://www.google.com/');
+    cy.get('textarea[type="search"]').type(Cypress.env('login'));
+    cy.screenshot('env', { capture: 'runner' })
 });
 });
