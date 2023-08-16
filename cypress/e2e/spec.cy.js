0f5f51b62e7a9805eaa85069e18a1b50077e0e09
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-16 12:59:16 +0000

    upd

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    
+    cy.wrap(Cypress.env('search'))
+    cy.log('**name:** ' + Cypress.env('search'));
     cy.visit('https://www.google.com/');
     cy.get('textarea[type="search"]').type(search)
   })
