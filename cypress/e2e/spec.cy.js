c07f7e72fc3964682b2551a75832d29417d6a6b0
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-16 17:08:08 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,5 +5,20 @@ describe('template spec', () => {
     console.log(Cypress.env())
     cy.visit('https://www.google.com/');
     cy.get('textarea[type="search"]').type('1')
-  })
-})
+  });
+  it('has valid env values', () => {
+  expect(Cypress.env())
+    .to.be.an('object')
+    .and.to.include.keys('person', 'location')
+  cy.wrap(Cypress.env('person'))
+    .should('have.keys', 'name', 'age')
+    .its('age')
+    .should('be.within', 10, 99)
+  cy.log('**name:** ' + Cypress.env('person').name)
+  cy.log('**city:** ' + Cypress.env('location').city)
+
+  // save a screenshot of the test runner
+  // to show what the env values were
+  cy.screenshot('env', { capture: 'runner' })
+});
+});
