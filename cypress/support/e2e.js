630904d533d7bb22e8fa69e343a18765637c8e7d
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 11:49:25 +0000

    up

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,6 +22,22 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     }
 });
 
+Cypress.on('window:before:load', function (window) {
+    const original = window.EventTarget.prototype.addEventListener
+  
+    window.EventTarget.prototype.addEventListener = function () {
+      if (arguments && arguments[0] === 'beforeunload') {
+        return
+      }
+      return original.apply(this, arguments)
+    }
+  
+    Object.defineProperty(window, 'onbeforeunload', {
+      get: function () { },
+      set: function () { }
+    })
+  });
+  
 if (Cypress.config('hideXHRInCommandLog')) {
     const app = window.top;
   
