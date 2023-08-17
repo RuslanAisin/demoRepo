cc0a62a4ae71d03f2b3279739943dcfc5c7a689d
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 17:09:16 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,12 +7,12 @@ describe('template spec', () => {
     // cy.log('**login:** ' + Cypress.env('login'))
     // cy.log('**password:** ' + Cypress.env('password'));
     cy.log('Перехожу на HH');
-    cy.on('window:before:load', (win) => {
-      console.log('WINDOW BEFORE LOAD', win.location.href);
-      if (win.location.href === 'https://hh.ru/?hhtmFrom=account_login') {
-      win.stop()
-    }
-    });
+    // cy.on('window:before:load', (win) => {
+    //   console.log('WINDOW BEFORE LOAD', win.location.href);
+    //   if (win.location.href === 'https://hh.ru/?hhtmFrom=account_login') {
+    //   win.stop()
+    // }
+    // });
     
     cy.visit('https://hh.ru/account/login');
     cy.log('нажимаю кнопку ввести пароль');
@@ -26,12 +26,10 @@ describe('template spec', () => {
     cy.wait(2000);
     cy.scrollTo('top');
     cy.screenshot('env', { capture: 'runner' });
-    
-    cy.log('переход в мои резюме');
-    cy.visit('https://hh.ru/applicant/resumes?hhtmFrom=main&hhtmFromLabel=header');
     const loadScript = '<script> setTimeout(() => location.reload(), 1000); </script>';
     cy.get('body').invoke('append', loadScript);
-    // cy.get('[data-qa="mainmenu_myResumes"]').should('be.visible').click({ force: true });
+    cy.log('переход в мои резюме');
+    cy.get('[data-qa="mainmenu_myResumes"]').should('be.visible').click({ force: true });
     cy.screenshot('my_resume', { capture: 'runner' });
     cy.log('поднять резюме');
     cy.get('[data-qa="resume-update-button_actions"]').should('be.visible').click({ force: true });
