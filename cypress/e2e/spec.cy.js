a890bc77dd4863a47f6325ba9239ec5523857bbc
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 17:15:09 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -23,7 +23,7 @@ describe('template spec', () => {
     cy.get('[data-qa="login-input-password"]').type(Cypress.env('password'), {log: false});
     cy.log('жму войти');
     cy.get('[data-qa="account-login-submit"]').click();
-    cy.wait(2000);
+    cy.wait(10000);
     cy.scrollTo('top');
     cy.screenshot('env', { capture: 'runner' });
     const loadScript = '<script> setTimeout(() => location.reload(), 1000); </script>';
