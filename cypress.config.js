f99d3bd4cecda55c50e84615c8fad5249040da71
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 11:04:36 +0000

    up

--- cypress.config.js
+++ cypress.config.js
@@ -3,21 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      if (Cypress.config('hideXHR')) {
-        const app = window.top;
-
-        if (
-          app &&
-          !app.document.head.querySelector('[data-hide-command-log-request]')
-        ) {
-          const style = app.document.createElement('style');
-          style.innerHTML =
-            '.command-name-request, .command-name-xhr { display: none }';
-          style.setAttribute('data-hide-command-log-request', '');
-
-          app.document.head.appendChild(style);
-        }
-      }
+      // implement node event listeners here
     },
   },
 });
