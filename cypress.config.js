f62f39d080de95e117e83f0d9ed55f5944d91b2a
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 11:10:42 +0000

    up

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,36 @@
 const { defineConfig } = require("cypress");
 
+if (Cypress.config('hideXHR')) {
+  const app = window.top;
+
+  if (
+    app &&
+    !app.document.head.querySelector('[data-hide-command-log-request]')
+  ) {
+    const style = app.document.createElement('style');
+    style.innerHTML =
+      '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+
+    app.document.head.appendChild(style);
+  }
+}
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        const width = 2400
+        const height = 1500
+        console.log('setting the browser window size to', width, height);
+        if (browser.name === 'chrome') {
+          launchOptions.args.push(`--window-size=${width},${height}`)
+          launchOptions.args.push('--force-device-scale-factor=1')
+        };
+        return launchOptions;
+      })
     },
   },
 });
