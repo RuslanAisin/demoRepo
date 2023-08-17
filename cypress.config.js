730a223b14bfe21ee45d916060ecb6a60a36f86e
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 16:16:04 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   hideXHRInCommandLog: true,
+  defaultCommandTimeout: 10000,
+  defaulpageLoadTimeout: 100000
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
@@ -24,5 +26,6 @@ module.exports = defineConfig({
         }
       })
     },
+    watchForFileChanges: false
   },
 });
