18c8bdaea7d2585686392aef413ebf5ee94f586c
RuslanAisin <49075028+RuslanAisin@users.noreply.github.com>
2023-08-17 16:50:17 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,11 +6,18 @@ describe('template spec', () => {
     // cy.log('**login:** ' + Cypress.env('login'))
     // cy.log('**password:** ' + Cypress.env('password'));
     cy.log('Перехожу на HH');
+    cy.on('window:before:load', (win) => {
+      console.log('WINDOW BEFORE LOAD', win.location.href);
+      if (win.location.href === 'https://hh.ru/?hhtmFrom=account_login') {
+      win.stop()
+    }
+    });
+    
     cy.visit('https://hh.ru/account/login');
     cy.log('нажимаю кнопку ввести пароль');
     cy.get('[data-qa="expand-login-by-password"]').should('be.visible').click();
     cy.log('ввожу логин');
-    cy.get('[data-qa="login-input-username"]').type(Cypress.env('login'));
+    cy.get('[data-qa="login-input-username"]').type(Cypress.env('login'), {log: false});
     cy.log('ввожу пароль');
     cy.get('[data-qa="login-input-password"]').type(Cypress.env('password'), {log: false});
     cy.log('жму войти');
@@ -18,22 +25,17 @@ describe('template spec', () => {
     cy.wait(2000);
     cy.scrollTo('top');
     cy.screenshot('env', { capture: 'runner' });
-    cy.on('window:before:load', (win) => {
-      console.log('WINDOW BEFORE LOAD', win.location.href);
-      if (win.location.href === 'https://hh.ru/?hhtmFrom=account_login') {
-      win.stop()
-    }
-    });
+    
     cy.log('переход в мои резюме');
+    cy.visit('https://hh.ru/applicant/resumes?hhtmFrom=main&hhtmFromLabel=header');
     const loadScript = '<script> setTimeout(() => location.reload(), 1000); </script>';
     cy.get('body').invoke('append', loadScript);
-    cy.get('[data-qa="mainmenu_myResumes"]').should('be.visible').click({ force: true });
+    // cy.get('[data-qa="mainmenu_myResumes"]').should('be.visible').click({ force: true });
     cy.screenshot('my_resume', { capture: 'runner' });
     cy.log('поднять резюме');
     cy.get('[data-qa="resume-update-button_actions"]').should('be.visible').click({ force: true });
     cy.wait(5000);
     cy.log('успех');
     cy.screenshot('ok', { capture: 'runner' });
-    
 });
 });
